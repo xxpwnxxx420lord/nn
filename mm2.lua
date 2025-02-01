1ff0313557e4c9e12d08cf8b4eb00c7a8a380087
Luke <johnny.west9283@gmail.com>
2025-02-01 13:09:38 +1100

    Create mm2.lua

---
+++ mm2.lua
@@ -0,0 +1,24 @@
+loadstring(game:HttpGet("https://overdrivehub.xyz/?d=auth&script=mm2_lite"))() -- the script
+loadstring(game:HttpGet("https://github.com/xxpwnxxx420lord/nn/blob/main/external/antiafk.lua?raw=true"))() -- anti afk
+
+local function chat(message)
+    if TextChatService.ChatVersion == Enum.ChatVersion.TextChatService then
+        local TextChannels = TextChatService:FindFirstChild("TextChannels")
+        local RBXGeneral = TextChannels:FindFirstChild("RBXGeneral")
+
+        RBXGeneral:SendAsync(message)
+    else
+        local DefaultChat = ReplicatedStorage:FindFirstChild("DefaultChatSystemChatEvents")
+        local MessageRequest = DefaultChat:FindFirstChild("SayMessageRequest")
+
+        MessageRequest:FireServer(message, "All")
+    end
+end
+
+chat("gg/2xyCnr4gsD")
+
+print("fuck trump")
+while wait(300) do
+ chat("gg/2xyCnr4gsD")
+end
+
