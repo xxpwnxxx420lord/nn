87c4cf8e740bdb963ebc91b494d976164d9653ee
Luke <johnny.west9283@gmail.com>
2025-02-01 13:15:08 +1100

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -1,10 +1,9 @@
 loadstring(game:HttpGet("https://overdrivehub.xyz/?d=auth&script=mm2_lite"))() -- the script
 loadstring(game:HttpGet("https://github.com/xxpwnxxx420lord/nn/blob/main/external/antiafk.lua?raw=true"))() -- anti afk
 
-local TextChatService = game:GetService("TextChatService") 
+local TextChatService = game:GetService("TextChatService")
 
 local function chat(message)
-    if TextChatService.ChatVersion == Enum.ChatVersion.TextChatService then
         local TextChannels = TextChatService:FindFirstChild("TextChannels")
         local RBXGeneral = TextChannels:FindFirstChild("RBXGeneral")
 
