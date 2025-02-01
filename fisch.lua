280fcf64b826bb191dc403d3811922cbdfe093b1
Luke <johnny.west9283@gmail.com>
2025-02-01 17:43:37 +1100

    Update fisch.lua

--- fisch.lua
+++ fisch.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/IdiotHubz/Scripts/refs/heads/main/Fisch/SpaceHub.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NYX8HUB/premium/refs/heads/main/script"))()
 loadstring(game:HttpGet("https://github.com/xxpwnxxx420lord/nn/blob/main/external/antiafk.lua?raw=true"))() -- anti afk
 
 local TextChatService = game:GetService("TextChatService")
